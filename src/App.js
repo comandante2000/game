b9e0041baf64819a1dce373ba165de8066bb318f
comandante2000 <joeycomandante19@gmail.com>
2022-06-22 16:15:58 -0800

    my game

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TicTacToe from './Components/TicTacToe';
+import React, { Component }  from 'react';
+function App() {
+  return (
+    <div className="App">
+     <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
